84aa7b043ce00dfe1f8d8878f601f04053fd551f
Fangyi Wang <fangyiwang@Fangyis-MacBook-Air.local>
2022-12-19 09:51:20 +1100

    update

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,22 @@
 import crpto from '../assets/projects/crpto.png'
-
+import canberra from '../assets/canberra.png'
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
+        name:"crpto project",
         image:crpto,
         github:"https://github.com/ucfangyiwang/cryptotracking",
         live:"https://ucfangyiwang.github.io/cryptotracking/",
     },
+    {
+        id:2,
+        name:"canberra model",
+        image:canberra,
+        github:"https://github.com/ucfangyiwang/project",
+        live:"https://ucfangyiwang.github.io/project/",
+    },
+    
     
 
 ]
