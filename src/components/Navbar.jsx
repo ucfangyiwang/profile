aebc41a0474965bf343f3c1f216c6a67bb4d1ed8
Fangyi Wang <fangyiwang@Fangyis-MacBook-Air.local>
2023-01-05 14:25:30 +1100

    color change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -103,7 +103,7 @@ const Navbar = () => {
       {/* Social icons */}
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
             <button
               className='flex justify-between items-center w-full text-gray-300'
               onClick={handleClick_Linkedin}
